7e37e516088931ab7ca307c7f0d2b8e8a4c0a267
Arifin-only <arifin180104@gmail.com>
2024-10-29 12:02:31 +0800

    List.dart

---
+++ List.dart
@@ -0,0 +1,30 @@
+void main(List<String> args){
+  
+  /*LIST
+    List<String> names = ["Upin", "Ipin", "Apin"];
+    
+    print(names[0]);
+    
+    GROWABLE LIST, jenis list yang ukurannya dapat bertambah secara dinamis
+    
+    List<int> number = [1, 2, 3];
+    
+    number.add(4);
+    
+    print(number);
+    
+    FIXED LIST
+  */
+  
+  List<String> fruits = List.filled(3, "");
+  
+  fruits[0] = "Apple";
+  fruits[1] = "Banana";
+  fruits[2] = "Cherry";
+  
+  print(fruits);
+  
+ /* fruits.add("Durian");
+  fruits.remove("Manggis");
+  
+  print(fruits[2]);*/

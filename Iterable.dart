17a048892cf57e749989257bdd166af3597c723a
Arifin-only <arifin180104@gmail.com>
2024-10-29 11:38:53 +0800

    Iterable.dart

---
+++ Iterable.dart
@@ -0,0 +1,11 @@
+void main(List<String> args){
+    //Iterable adalah jenis data yang bisa diulang satu per satu,
+    //seperti daftar barang atau uruta angka. contoh dari iterable
+    //adalah list dan set
+    List<int> number = [1, 2, 3, 4, 5];//contoh dari iterable
+    
+    //menggunakan for-each untuk mengitegrasi setiap elemen
+    for (int number in numbers){
+        print(number);
+    }
+}
